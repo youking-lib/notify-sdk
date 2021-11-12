8c9039422e17c45f3fcecda0516b79dde6c4071a
yongzhen <yongzhen@kuaishou.com>
2021-11-12 10:56:31 +0800

    feat: 测试 sdk 与组件通信

---
+++ src/sdk/DrawerContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-button @click="onClose">测试点击</a-button>
+</template>
+
+<script>
+import { Button } from 'ant-design-vue';
+export default {
+  components: {
+    Button
+  },
+  methods: {
+    onClose () {
+      this.$emit('close');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/sdk/drawer.jsx
+++ src/sdk/drawer.jsx
@@ -1,4 +1,6 @@
+import {} from "vue";
 import { notification } from "ant-design-vue";
+import DrawerContent from "./DrawerContent.vue";
 
 export default class Drawer {
   static DRAWER_KEY = "Drawer_Key";
@@ -7,7 +9,7 @@ export default class Drawer {
     notification.open({
       key: Drawer.DRAWER_KEY,
       message: "Notification Title",
-      description: <iframe src="http://localhost:3000/index_frame.html" />,
+      description: <DrawerContent onClose={() => console.log(123)} />,
       duration: null,
       style: {
         width: "300px",
